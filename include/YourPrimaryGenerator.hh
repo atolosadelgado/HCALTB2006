94946b52ba5b67989f69dddc5acd6ac655a57aeb
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2025-12-05 11:13:25 +0100

    refactor primary generator, to include GPS or particle gun

--- include/YourPrimaryGenerator.hh
+++ include/YourPrimaryGenerator.hh
@@ -1,32 +1,34 @@
-#ifndef YOURPRIMARYGENERATOR
-#define YOURPRIMARYGENERATOR
+#ifndef YOURPRIMARYGENERATOR_HH
+#define YOURPRIMARYGENERATOR_HH
 
-#include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4ParticleGun.hh"
-#include "G4Event.hh"
+#include <G4VUserPrimaryGeneratorAction.hh>
+#include <G4VPrimaryGenerator.hh>
+#include <G4ParticleGun.hh>
+#include <G4GeneralParticleSource.hh>
+#include <CLHEP/Units/SystemOfUnits.h>
 
 class YourPrimaryGenerator : public G4VUserPrimaryGeneratorAction
 {
 public:
-    YourPrimaryGenerator(): G4VUserPrimaryGeneratorAction() { fParticleGun = new G4ParticleGun(1); }
-    ~YourPrimaryGenerator(){delete fParticleGun;}
-    virtual void GeneratePrimaries(G4Event* anEvent){
-        // if( counter % 10 == 0)
-        // std::cout << "PrimaryGeneratorAction. Event " << counter << std::endl;
-        // counter++;
-        fParticleGun->GeneratePrimaryVertex(anEvent);
-        x0_mm = anEvent->GetPrimaryVertex(0)->GetPosition().x() / CLHEP::mm;
-        y0_mm = anEvent->GetPrimaryVertex(0)->GetPosition().y() / CLHEP::mm;
-        E0_MeV = anEvent->GetPrimaryVertex(0)->GetPrimary()->GetKineticEnergy() / CLHEP::MeV;
-    }
+    enum class SourceType {
+        kParticleGun,
+        kGPS
+    };
+
+    YourPrimaryGenerator(SourceType type = SourceType::kGPS);
+    virtual ~YourPrimaryGenerator();
+
+    virtual void GeneratePrimaries(G4Event* event);
+
     G4double x0_mm{-999.};
     G4double y0_mm{-999.};
     G4double E0_MeV{-999.};
 
 private:
-    G4ParticleGun* fParticleGun;
-    int counter = {0};
+    G4VPrimaryGenerator* fPrimaryGen = nullptr;  // ← único puntero
+    SourceType fType;
+
+    int counter = 0;
 };
 
 #endif
-
---
+++ src/YourPrimaryGenerator.cc
@@ -0,0 +1,28 @@
+#include "YourPrimaryGenerator.hh"
+#include <iostream>
+
+YourPrimaryGenerator::YourPrimaryGenerator(SourceType type)
+: G4VUserPrimaryGeneratorAction(), fType(type)
+{
+    if (fType == SourceType::kParticleGun) {
+        fPrimaryGen = new G4ParticleGun(1);
+    } else {
+        fPrimaryGen = new G4GeneralParticleSource();
+    }
+}
+
+YourPrimaryGenerator::~YourPrimaryGenerator()
+{
+    delete fPrimaryGen;
+}
+
+void YourPrimaryGenerator::GeneratePrimaries(G4Event* event)
+{
+    fPrimaryGen->GeneratePrimaryVertex(event);
+
+    auto vertex = event->GetPrimaryVertex(0);
+    x0_mm = vertex->GetPosition().x() / CLHEP::mm;
+    y0_mm = vertex->GetPosition().y() / CLHEP::mm;
+    E0_MeV = vertex->GetPrimary()->GetKineticEnergy() / CLHEP::MeV;
+}
+
