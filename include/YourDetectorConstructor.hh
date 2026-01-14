7cc356712cd1dc7c06b1524928339d6cf2fd967b
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-01-14 17:03:30 +0100

    disable ECAL in Detector constructor + ctest

--- include/YourDetectorConstructor.hh
+++ include/YourDetectorConstructor.hh
@@ -4,6 +4,9 @@
 
 #include "G4VUserDetectorConstruction.hh"
 
+class G4Material;
+class G4VPhysicalVolume;
+
 /// Detector construction allowing to use the geometry read from the GDML file
 class YourDetectorConstructor : public G4VUserDetectorConstruction
 {
@@ -13,8 +16,14 @@ class YourDetectorConstructor : public G4VUserDetectorConstruction
   ~YourDetectorConstructor() override;
   virtual G4VPhysicalVolume* Construct() override;
 
+  void ConfigureECALAsAir(){ECALAsAir=true;}
+
   private:
       std::string gdml_filename;
+      G4VPhysicalVolume * worldPV;
+      bool ECALAsAir = false;;
+      void MakeECALAsAir();
+      void ReplaceMaterialInTree(G4VPhysicalVolume* rootPV, G4Material* newMat);
 };
 
 #endif
--- src/YourDetectorConstructor.cc
+++ src/YourDetectorConstructor.cc
@@ -17,12 +17,15 @@ YourDetectorConstructor::YourDetectorConstructor(std::string fname) :  G4VUserDe
 G4VPhysicalVolume * YourDetectorConstructor::Construct(){
   G4GDMLParser Parser;
   Parser.Read(gdml_filename, false);
-  auto worldPV = Parser.GetWorldVolume();
+  worldPV = Parser.GetWorldVolume();
 
   G4Material* scint = G4Material::GetMaterial("Scintillator");
   scint->GetIonisation()->SetBirksConstant(0.006*CLHEP::mm/CLHEP::MeV);
   G4LossTableManager::Instance()->EmSaturation()->InitialiseG4Saturation();
 // G4LossTableManager::Instance()->EmSaturation()->DumpBirksCoefficients();
+
+  if(ECALAsAir)
+    this->MakeECALAsAir();
 
   return worldPV;
 }
@@ -30,4 +33,66 @@ G4VPhysicalVolume * YourDetectorConstructor::Construct(){
 
 YourDetectorConstructor::~YourDetectorConstructor(){
 
+}
+
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4GeometryManager.hh"
+
+void YourDetectorConstructor::MakeECALAsAir()
+{
+
+  G4VPhysicalVolume* ecalPV = nullptr;
+
+  // World -> TBHCal -> Calo (+HcalTestBeamLine, walls, etc) ->
+  G4LogicalVolume* motherLV = G4LogicalVolumeStore::GetInstance()->GetVolume("Calo");
+
+  if(nullptr == motherLV) throw std::runtime_error("YourDetectorConstructor::SetupECALAsAir cannot find Calo LV");
+
+  for (int i = 0; i < motherLV->GetNoDaughters(); i++) {
+      G4VPhysicalVolume* daughter = motherLV->GetDaughter(i);
+      std::cout << "World daughter : " << daughter->GetName() << std::endl;
+      if (daughter->GetName() == "ECAL") { // nombre del physvol del ECAL
+          ecalPV = daughter;
+          break;
+      }
+  }
+
+  if(nullptr == ecalPV) throw std::runtime_error("YourDetectorConstructor::SetupECALAsAir cannot find ECAL physical volume");
+  else       G4cout << "Found ECAL physvol at position: " << ecalPV->GetTranslation() << G4endl;
+  G4Material * air_mat = G4Material::GetMaterial("Air");
+  if(nullptr == air_mat) throw std::runtime_error("YourDetectorConstructor::SetupECALAsAir cannot find Air material");
+
+  ReplaceMaterialInTree(ecalPV, air_mat);
+
+}
+
+#include <queue>
+#include <set>
+
+void YourDetectorConstructor::ReplaceMaterialInTree(G4VPhysicalVolume* rootPV,
+                            G4Material* newMat)
+{
+  std::queue<G4VPhysicalVolume*> queue;
+  std::set<G4LogicalVolume*> visited;
+
+  queue.push(rootPV);
+
+  while (!queue.empty())
+  {
+    auto pv = queue.front();
+    queue.pop();
+
+    auto lv = pv->GetLogicalVolume();
+
+    if (visited.insert(lv).second)
+    {
+      lv->SetMaterial(newMat);
+    }
+
+    for (int i = 0; i < lv->GetNoDaughters(); ++i)
+    {
+      queue.push(lv->GetDaughter(i));
+    }
+  }
 }
