c759e230afd4d3b8ed00a748c063fd894b43ec68
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-02-19 12:59:37 +0100

    add CMS hadronic physics

--- include/CMSHadronPhysicsFTFP_BERT.hh
+++ include/CMSHadronPhysicsFTFP_BERT.hh
@@ -1,16 +1,23 @@
 #ifndef CMSHadronPhysicsFTFP_BERT_hh
 #define CMSHadronPhysicsFTFP_BERT_hh
 
-#include "G4VPhysicsConstructor.hh"
 #include "globals.hh"
+#include "G4HadronPhysicsFTFP_BERT.hh"
 
-class CMSHadronPhysicsFTFP_BERT : public G4VPhysicsConstructor
-{
-  public:
-    CMSHadronPhysicsFTFP_BERT(double minFTFP, double maxBERT, double maxBERTpi, double minFTFP_2, double maxBERT_2);
-    ~CMSHadronPhysicsFTFP_BERT() override = default;
-    void ConstructParticle() override {}
-    void ConstructProcess() override {};
+class CMSHadronPhysicsFTFP_BERT : public G4HadronPhysicsFTFP_BERT {
+public:
+  explicit CMSHadronPhysicsFTFP_BERT(G4int verb);
+  explicit CMSHadronPhysicsFTFP_BERT(G4double e1, G4double e2, G4double e3, G4double e4, G4double e5);
+  ~CMSHadronPhysicsFTFP_BERT() override = default;
+
+  void ConstructProcess() override;
+
+  void Neutron() override;
+
+  // copy constructor and hide assignment operator
+  CMSHadronPhysicsFTFP_BERT(CMSHadronPhysicsFTFP_BERT &) = delete;
+  CMSHadronPhysicsFTFP_BERT &operator=(const CMSHadronPhysicsFTFP_BERT &right) = delete;
 };
+
 
 #endif
