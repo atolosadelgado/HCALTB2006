937fe1b07b0622ee1d321de206e9393a24a138b3
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-03-17 13:35:19 +0100

    clean event action header

--- include/YourEventAction.hh
+++ include/YourEventAction.hh
@@ -13,21 +13,21 @@ public:
   void EndOfEventAction(const G4Event*) override;
   void BeginOfEventAction(const G4Event*) override;
   void SetPrimaryGenerator(YourPrimaryGenerator * p){fPrimaryGenerator=p;}
-  void UpdateTime(double t);
-  double GetEventTime(){return time_last_hit - time_first_hit; }
-  void ResetEventTime(){
-    time_first_hit = {std::numeric_limits<double>::max()};
-    time_last_hit = {-std::numeric_limits<double>::max()};
-  };
-
-    std::vector<double> fGammaE0_ecal;
-    std::vector<int> fGammaModelIndex_ecal;
-    std::vector<double> fNeutronEfinal;
-    std::vector<double> fNeutronTfinal;
+  // void UpdateTime(double t);
+  // double GetEventTime(){return time_last_hit - time_first_hit; }
+  // void ResetEventTime(){
+  //   time_first_hit = {std::numeric_limits<double>::max()};
+  //   time_last_hit = {-std::numeric_limits<double>::max()};
+  // };
+  //
+  //   std::vector<double> fGammaE0_ecal;
+  //   std::vector<int> fGammaModelIndex_ecal;
+  //   std::vector<double> fNeutronEfinal;
+  //   std::vector<double> fNeutronTfinal;
 private:
     YourPrimaryGenerator * fPrimaryGenerator;
-    double time_first_hit = {std::numeric_limits<double>::max()};
-    double time_last_hit = {-std::numeric_limits<double>::max()};
+    // double time_first_hit = {std::numeric_limits<double>::max()};
+    // double time_last_hit = {-std::numeric_limits<double>::max()};
 };
 
 #endif
