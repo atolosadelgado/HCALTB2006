e79f71292c34dfdd4be91cf7b4a78e2fb212ce1d
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-03-17 08:31:04 +0100

    clean ECAL

--- include/ECalSD.h
+++ include/ECalSD.h
@@ -1,28 +1,20 @@
 #ifndef ECalSD_h
 #define ECalSD_h
 
-// #include "ECalSD_config.h"
-
 #include <string>
 
 #include "G4VSensitiveDetector.hh"
 
-#include "YourPrimaryGenerator.hh"
-
 #include "EnergyAccumulatorPerLV.hh"
+
+class YourPrimaryGenerator;
 
 /* class ECalSD
  * reimplements CMSSW classes
  * EcalSD -> CaloSD -> SensitiveCaloDetector -> SensitiveDetector
- * in one single class
- * Ignoring at the moment:
- *      EnergyCorrected, getTrackID, processSecondHit
- *      initMap, getRadiationLength, getLayerIDForTimeSim
- *      Birks / slope / weighting / ageing
- *      maps per cell (hitMap, CaloHitID)
- *      fillHits (EDM), drawAll, printAll
- *
+ * in one single class. Birk correction placed in SDutils namespace
  */
+
 class ECalSD : public G4VSensitiveDetector {
 public:
     ECalSD(std::string fname):G4VSensitiveDetector(fname){ std::cout << "New EcalSD named " << fname.c_str() << std::endl; }
@@ -57,53 +49,16 @@ public:
     std::vector<G4int> fModelIndex_gamma;
 
     int verbosity = {0};
-    // void UpdatePrimaryEnergy(double e){ /*primary_energy = std::max(e,primary_energy);*/ if(0>primary_energy)primary_energy=e;}
-    // double GetPrimaryEnergy(){return primary_energy;}
-    // void ResetPrimaryEnergy(){primary_energy=-999;}
-    // double primary_energy = {-999.};
+
     void UpdatePrimaryEnergy(double e){primary_energy = std::max(e,primary_energy);}
     double GetPrimaryEnergy(){return primary_energy;}
     void ResetPrimaryEnergy(){primary_energy={-std::numeric_limits<double>::max()};}
     double primary_energy = {-std::numeric_limits<double>::max()};
     EnergyAccumulatorPerLV energy_accumulator;
-    // hardcoded members of ECalSD + CaloSD for ECal, as in CMSSW
-    // ECalSD_config cms_config;
-
-    // functions from ECalSD, as in CMSSW
-    // void getBaseNumber(const G4Step *);
-    // double getBirkL3(const G4Step *);
-    // method getDepth calculates too many things, reimplemented as computeCrystalDepth
-    // uint16_t getDepth(const G4Step *);
-    // double computeCrystalDepth(const G4Step* aStep);
-    // double getEnergyDeposit(const G4Step* aStep);
-    // uint32_t setDetUnitId(const G4Step *) ;
-    // double curve_LY(const G4LogicalVolume* lv, const G4Step * step);
-
-    // Methods not migrated:
-    // EcalBarrelNumberingScheme::getUnitID
-    // EcalBaseNumber::addLevel
-    // EcalBaseNumber::getCopyNumber
 
     // Methods from CaloSD, as in CMSSW,
     virtual G4bool ProcessHits(G4Step* aStep, G4TouchableHistory* ROhist) override;
     YourPrimaryGenerator * fPrimaryGenerator;
-
-    // virtual int getTrackID(const G4Track*);
-    // function to deal with hits, not needed here
-    // struct CaloG4Hit{};
-    // bool checkHit(int k = 0);
-    // void cleanHitCollection();
-    // CaloG4Hit* createNewHit(const G4Step*, const G4Track*, int k);
-    // bool hitExists(const G4Step*, int k);
-    // bool saveHit(CaloG4Hit*, int k = 0);
-    // ??
-    // void update(const BeginOfRun*) override;
-    // void update(const BeginOfEvent*) override;
-    // void update(const BeginOfTrack* trk) override;
-    // void update(const EndOfTrack* trk) override;
-    // void update(const ::EndOfEvent*) override;
-
-
 
   // LV attached to ECalSD (dumped from CMSSW)
   std::vector<std::string> sensitive_lv = { "EBRY_01_refl",
@@ -141,12 +96,6 @@ public:
                                             "EBRY_17",
                                             "EBRY_17_refl"
                                         }; // end sensitive_lv
-
-
-
-
-
-
 
 };
 #endif
