2a53d94bf7b798c645a3b85f1b8f8979de3df7a4
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-08-26 16:30:24 +0200

    update files needed to submit to htcondor

--- condor/run.sh
+++ condor/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # load LCG
-source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc13-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_106b/x86_64-el9-gcc13-opt/setup.sh
 
-echo "LCG 105 loaded, Geant4 version 11.2.0"
+echo "LCG version: ${LCG_VERSION}"
+echo "Geant4 version $(geant4-config --version)"
 echo "Running on $(hostname)"
 echo "Args: $@"
 
