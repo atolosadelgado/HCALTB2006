9ca9e03395a39832ecab52e98484d89bd2d63c69
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2026-08-28 16:49:05 +0200

    create plots in root

--- plotting/calibrate.py
+++ plotting/calibrate.py
@@ -127,6 +127,7 @@ def plot_calibration(filename, detector):
     # ------------------------------------------------------------------------
 
     canvas.SaveAs(f"{detector}_calibration.png")
+    canvas.SaveAs(f"{detector}_calibration.root")
 
 
 # ============================================================================
